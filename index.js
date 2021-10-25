9c889c93e4766bb9a58f2665d51115a97c7f8a4b
Sartaj Mahmud <oishik1999@gmail.com>
2021-10-26 02:49:51 +0600

    Need to work on update and create queries for attendance

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ var con = mysql.createConnection({
    password: "",
    database: "erp_attendance"
 });
+con.connect(function(err) {
+   if (err) {
+      console.error('[MySQL] Error connecting: ' + err.stack);
+      return;
+   }
+   console.log('[MySQL] Connected as id ' + con.threadId);
+});
 
 //const server = require('http').createServer(app);
 const  server = app.listen(5000,function (){
@@ -23,19 +30,131 @@ wss.on('connection', function connection(ws){
    ws.send('Welcome New Client!');
 
    ws.on('message', function incoming(message){
+
+      let date_ob = new Date();
+
+      // current date
+      // adjust 0 before single digit date
+      // let date = ("0" + date_ob.getDate()).slice(-2);
+      // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+      // let year = date_ob.getFullYear();
+      // let hours = date_ob.getHours();
+      // let minutes = date_ob.getMinutes();
+      // let seconds = date_ob.getSeconds();
+      // let currentTime = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+
+      var str = message.toString();
+      let parts = str.split(",");
+      console.log(parts);
+      var lat = parts[1];
+      var lon = parts[2];
+      var ssid = parts[3];
+      var userData;
+      var userAssignedLocationData;
+      var userAssignedNetworkData;
+      var mapAPIKey = 'AIzaSyBzQ7shRAOAFAdZtIpYp9bNnQnFerPgotw';
+
+      var userQuery = "SELECT * FROM users WHERE id ="+parts[0]+" ";
+      con.query(userQuery, function (err, result, fields) {
+         if (err) throw err;
+         userData = result[0];
+         console.log(userData.id);
+      });
+
+      var locationQuery = "SELECT * FROM locations WHERE id = 6";
+      con.query(locationQuery, function (err, result, fields) {
+         if (err) throw err;
+         userAssignedLocationData = result[0];
+         console.log(result[0].id);
+      });
+
+      var networkQuery = "SELECT * FROM networks WHERE id = 5";
+      con.query(networkQuery, function (err, result, fields) {
+         if (err) throw err;
+         userAssignedLocationData = result[0];
+         console.log(result[0].id);
+      });
+
+      // con.connect(function(err) {
+      //    if (err) throw err;
+      //    console.log("Connected To mysql!");
+      //    var userQuery = "SELECT * FROM users WHERE id ="+parts[0]+" ";
+         // con.query(userQuery, function (err, result, fields) {
+         //    if (err) throw err;
+         //    userData = result[0];
+         //    console.log(userData.attendance_type);
+            // var locationQuery = "SELECT * FROM locations WHERE id ="+userData.location_id+" ";
+            // con.query(locationQuery, function (err, result, fields) {
+            //    if (err) throw err;
+            //    userAssignedLocationData = result[0];
+            //    console.log(result);
+            // });
+            // var networkQuery = "SELECT * FROM networks WHERE id ="+userData.network_id+" ";
+            // con.query(networkQuery, function (err, result, fields) {
+            //    if (err) throw err;
+            //    userAssignedNetworkData = result[0];
+            //    console.log(result);
+               // if(userData.attendance_type == 1){
+               //
+               // }else if(userData.attendance_type == 2){
+               //    console.log(userAssignedNetworkData.ssid);
+               //    if(userAssignedNetworkData.ssid == parts[3]){
+               //       console.log('matched');
+               //       var attendance = "SELECT * FROM attendances WHERE user_id ="+userData.id+" ";
+               //       con.query(attendance, function (err, result, fields) {
+               //          if (err) throw err;
+               //          console.log(result);
+               //          if(result.length == 0){
+               //             var newEntryQuery = "INSERT INTO attendances (user_id, entry_time, created_at, updated_at) VALUES( "+userData.id+", now(), now(), now() )";
+               //             con.query(newEntryQuery, function (err, result, fields){
+               //                if (err) throw err;
+               //                console.log(result);
+               //                console.log('Entry Successful');
+               //             });
+               //          }else if(result[0].entry_time == null){
+               //
+               //          }else{
+               //
+               //          }
+               //       });
+               //    }else{
+               //       var attendance = "SELECT * FROM attendances WHERE user_id ="+userData.id+" ";
+               //       con.query(attendance, function (err, result, fields) {
+               //          if (err) throw err;
+               //          console.log(result.length);
+               //          if(result.length != 0){
+               //             console.log("exit"+result[0].exit_time+'12'+result[0].exit_time.length);
+               //             if(result[0].exit_time != null && result[0].exit_time.length != 0){
+               //                console.log('already exists');
+               //             }else if (result[0].entry_time != null){
+               //                var exitUpdateQuery = "UPDATE attendances SET exit_time = NOW(), updated_at = now() WHERE user_id = "+userData.id;
+               //                con.query(exitUpdateQuery, function (err, result, fields){
+               //                   if (err) throw err;
+               //                   console.log('Entry Successful');
+               //                });
+               //             }else{
+               //                console.log('Entry first1')
+               //             }
+               //          }else{
+               //             console.log('Entry first2');
+               //          }
+               //
+               //       });
+               //    }
+               //
+               // }
+           // });
+
+
+
+       //  });
+
+     // });
       console.log('received: %s', message);
       ws.send('Got your message its: '+message);
    });
+   console.log("Total client : "+wss.clients.size);
 });
 
 
 
-
-con.connect(function(err) {
-   if (err) throw err;
-   console.log("Connected!");
-   con.query("SELECT * FROM users", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-   });
-});
